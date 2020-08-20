5a35399e47b485333164431714c2c229baf829fd
af81392 <rajesh.kotari@anthem.com>
2020-08-20 15:41:36 -0700

    adding random dealy

--- src/divide.py
+++ src/divide.py
@@ -1,6 +1,11 @@
 import sys
+from time import sleep
+import random
 
 def divide(x, y):
+    rdm = random.randint(1,10)
+    print('sleeping for - ' + str(rdm) + ' seconds.')
+    sleep(rdm)
     return int(x) / int(y)
 
 
