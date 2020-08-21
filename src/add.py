2720c9ad90258e294f24e300b28a99410b1cf703
af81392 <rajesh.kotari@anthem.com>
2020-08-20 19:14:39 -0700

    listing directories

--- src/add.py
+++ src/add.py
@@ -8,6 +8,8 @@ def add(x, y):
     print('sleeping for - ' + str(rdm) + ' seconds.')
     sleep(rdm)
     sum = int(x) + int(y)
+    arr = os.listdir('/workspace')
+    print(arr)
     f = open("/workspace/sum.txt","w+")
     f.write(str(sum))
     os.environ['results.sum'] = str(sum)
