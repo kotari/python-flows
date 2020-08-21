40649315bb03fa3b06f32edd0548d83e9cb3642a
af81392 <rajesh.kotari@anthem.com>
2020-08-20 18:14:54 -0700

    printing env variable

--- src/add.py
+++ src/add.py
@@ -1,11 +1,13 @@
 import sys
 from time import sleep
 import random
+import os
 
 def add(x, y):
     rdm = random.randint(1,10)
     print('sleeping for - ' + str(rdm) + ' seconds.')
     sleep(rdm)
+    print(os.env['results'])
     return int(x) + int(y)
 
 
