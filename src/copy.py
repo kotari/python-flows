e7c92af84168864c2d03bbd0581a5e3b42185781
af81392 <rajesh.kotari@anthem.com>
2020-08-22 19:00:26 -0700

    adding logic to copy results to minio

---
+++ src/copy.py
@@ -0,0 +1,14 @@
+from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+import os
+
+mc = Minio(os.environ['MINIO_SERVICE_HOST']+ ':' + os.environ['MINIO_SERVICE_PORT'],
+                    access_key='minio',
+                    secret_key='minio_password',
+                    secure=False)
+
+if __name__ == '__main__':
+    print(sys.argv)
+    # mc.fput_object('mat', 'result.txt', '/workspace/filedrop/result.txt')
+    print(mc.fput_object('mat', sys.argv[1], sys.argv[2]))
